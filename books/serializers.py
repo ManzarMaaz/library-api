a0970c189f69454aeabd88fd0b0aa8c351cc87bd
Mohammad Manzar Maaz <themmm2003@gmail.com>
2026-05-21 13:47:47 +0530

    feat: implement DRF and BookSerializer for books endpoint

---
+++ books/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'published_date']
--- books/views.py
+++ books/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def book_list_api(request):
     """API endpoint to return a list of books in JSON format."""
 
     books = Book.objects.select_related('author').all()
 
-    data = []
-    for book in books:
-        data.append({
-            "title": book.title,
-            "isbn": book.isbn,
-            "author": book.author.name, # No extra DB hit because of select_related!
-            "published_date": book.published_date.strftime('%Y-%m-%d') if book.published_date else None
-        })
+    serializer = BookSerializer(books, many=True)
 
-    return JsonResponse({"status": "success", "data": data})
+    return Response(serializer.data)
